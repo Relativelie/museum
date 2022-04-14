af928f04d292baba6488e171bc77a185f37c3d94
Relativelie <sopio1992@gmail.com>
2022-04-15 01:17:07 +0300

    commit

--- src/components/feedback/time/timeValues.ts
+++
@@ -1,6 +0,0 @@
-const currentDate = new Date();
-
-const hours = currentDate.getHours().toString().length === 2 ? currentDate.getHours() : `0${currentDate.getHours()}`;
-const minutes = currentDate.getMinutes().toString().length === 2 ? currentDate.getMinutes() : `0${currentDate.getMinutes()}`;
-
-export const time = `${hours}:${minutes}`;
